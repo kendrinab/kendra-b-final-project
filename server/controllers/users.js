7b2d195da3e501a4a1ff0df6bc187b0eb101425a
kendrinab <kendra.barrera01@gmail.com>
2020-12-09 12:45:16 -0500

    added controllers (users.js)

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,11 +4,170 @@ const User = require('../db/models/User'),
     sendWelcomeEmail,
     sendCancellationEmail,
     forgotPasswordEmail
-  } = require('../');
+  } = require('../emails/index'),
+  jwt = require('jsonwebtoken');
 
-// exports.createUser = async (req, res) => {
-//   const { name, email, password } req.body;
-//   try {
-//     const {name}
-//   }
-// }
+// ***********************************************//
+// CREATE A USER
+// ***********************************************//
+
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({
+      name,
+      email,
+      password
+    });
+    sendWelcomeEmail(user.email, user.name);
+    const token = await user.generateAuthToken();
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      sameSite: 'Strict',
+      secure: process.env.NODE_ENV !== 'production' ? false : true
+    });
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+// ***********************************************//
+// LOGIN A USER
+// ***********************************************//
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      sameSite: 'Strict',
+      secure: process.env.NODE_ENV !== 'production' ? false : true
+    });
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+// ******************************
+// PASSWORD RESET REQUEST
+// This route sends an email that the
+// user must click within 10 minutes
+// to reset their password.
+// ******************************
+
+exports.requestPasswordReset = async (req, res) => {
+  try {
+    const { email } = req.query;
+    const user = await User.findOne({ email });
+    if (!user) throw new Error('Uh Oh! User Not Found');
+    const token = jwt.sign(
+      { _id: user._id.toString(), name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: '10m' }
+    );
+    forgotPasswordEmail(email, token);
+    res.json({ message: 'Password Reset Email Has been Sent!' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+exports.passwordRedirect = async (req, res) => {
+  const { token } = req.params;
+  try {
+    jwt.verify(token, process.env.JWT_SECRET, function (err) {
+      if (err) throw new Error(err.message);
+    });
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      maxAge: 600000,
+      sameSite: 'Strict'
+    });
+    res.redirect(process.env.URL + '/update=password');
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// ***********************************************//
+// GET A CURRENT USER
+// ***********************************************//
+
+exports.getCurrentUser = async (req, res) => {
+  res.json(req.user);
+};
+
+// ***********************************************//
+// UPDATE A USER
+// ***********************************************//
+
+exports.updateCurrentUser = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'avatar'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).json({ message: 'Invalid updates' });
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.json(req.user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// ***********************************************//
+// LOGOUT A USER
+// ***********************************************//
+
+exports.logoutUser = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.cookies.jwt;
+    });
+    await req.user.save();
+    res.clearCookie('jwt');
+    res.json({ message: 'You have successfully logged out!' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// ***********************************************//
+// LOGOUT ALL DEVICES
+// ***********************************************//
+
+exports.logoutAllDevices = async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.clearCookie('jwt');
+    res.json({ message: 'You have successfully logged out from all devices!' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// ***********************************************//
+// DELETE A USER
+// ***********************************************//
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await req.user.remove();
+    sendCancellationEmail(req.user.email, req.user.name);
+    res.clearCookie('jwt');
+    res.json({ message: 'This user has successfully been deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// ***********************************************//
+// UPLOAD AN AVATAR
+// ***********************************************//
