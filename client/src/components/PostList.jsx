f7d86e8b83426b3cbefdb7f863613705e1b45d08
kendrinab <kendra.barrera01@gmail.com>
2020-12-15 01:47:55 -0500

    added Post List

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Container, Table } from 'react-bootstrap';
+import Post from './Post';
+import axios from 'axios';
+import { AppContext } from '../context/AppContext';
+import Search from './Search';
+import swal from 'sweetalert';
 
 const PostList = () => {
-  return <div></div>;
+  const {
+    setPosts,
+    search,
+    filteredPosts,
+    setFilteredPosts,
+    loading
+  } = useContext(AppContext);
+  useEffect(() => {
+    axios
+      .get('/api/posts?sortBy=dueDate:asc', { withCredentials: true })
+      .then((response) => {
+        setTasks(response.data);
+        setFilteredTasks(response.data);
+      })
+      .catch((error) => {
+        swal(`Oops!`, 'Something went wrong.');
+      });
+  }, [setPosts, setFilteredPosts, search, loading]);
+  return (
+    <Container>
+      <Search />
+      <Table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <Post posts={filteredPosts} />
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default PostList;
