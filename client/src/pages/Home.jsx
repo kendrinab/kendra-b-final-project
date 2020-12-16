ca6d9778085290a3b17b19b52b9f9d53348af214
kendrinab <kendra.barrera01@gmail.com>
2020-12-15 20:59:47 -0500

    adjusted CSS in Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,26 +4,25 @@ import WaveImage from '../images/wave.png';
 const Home = () => {
   return (
     <div id="homepage">
-      <h1>CLN +ALT Blog</h1>
+      {/* <h4>CLN +ALT Blog</h4> */}
       <section>
         <div className="top-container">
-          <h1>Think Cleaner!</h1>
-          <h6>
-            The mission here is to create a safer, and much more sustainable
-            personal care industry, to provide people helpful articles on
-            products they might not know is harming them.
-          </h6>
+          <h1>Think Clean!</h1>
+          <h5 className="top-home-h6">
+            Our Mission is to create a safer, and much more sustainable personal
+            care industry, to provide people helpful articles on products they
+            might not know is harming them.
+          </h5>
           <img src={WaveImage} alt="" className="curved-photo" />
         </div>
       </section>
       <section>
         <div className="home-info">
           <section>
-            <p className="p1">
-              Hi! Welcome to the CLN +ALT Blog. The place to find nothing but
-              informative & creative posts on Bodycare, Skincare, Haircare, and
-              so much more!
-            </p>
+            <h5 className="p1">
+              CLN +ALT Blog, The place to find nothing but informative &
+              creative posts on Bodycare, Skincare, Haircare, and so much more!
+            </h5>
           </section>
         </div>
       </section>
